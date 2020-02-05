191abaf8bd8b625672861fe7cf7056ea23816fa2
NozomuOhashi <ohashi@nagoya-u.jp>
2020-02-05 14:48:26 -0800

    mks

--- src/packages/mypkg/cooperation_setup/run/mks_setup.sh
+++ src/packages/mypkg/cooperation_setup/run/mks_setup.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-htpdate 192.168.0.102
-ntpdate -q 192.168.0.102
+#htpdate 192.168.0.102
+#ntpdate -q 192.168.0.102
 
 read -p "Is time sync ok? : "
 
-xterm -e bash -c "roslaunch cooperation_setup mks_driver.launch" &
+xterm -e bash -c "source ~/ohashi/test_perception/devel/setup.bash; roslaunch cooperation_setup mks_driver.launch" &
 sleep 1
 
 read -p "Hit enter after microstrain_3dm_gx5_45_node is correctly launched: "
 
-xterm -e bash -c "roslaunch cooperation_setup mks_setup.launch" &
+xterm -e bash -c "source ~/ohashi/test_perception/devel/setup.bash; roslaunch cooperation_setup mks_setup.launch" &
 sleep 1
 
